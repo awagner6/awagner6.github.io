c7ffc81a8748c6e72cb9516f9a43777a9eeb639f
awagner6 <37525104+awagner6@users.noreply.github.com>
2024-03-29 23:12:06 -0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -4,14 +4,17 @@ export const puzzles = [
         solution: ["FIRE TRUCK", "PUMPKIN", "BLONDE", "CROCODILE", "SKY", "DENIM", "LAVENDER"],
         initialOrderSequence: "6243175",
         theme: "ROYGBIV",
-        hint: ""
+        hint: "Take a bow",
+        hint2: "Get colorful"
     },
     {
         id: 2,
         solution: ["SNAKE", "FROG", "STARFISH", "ANT", "SPIDER", "CRAB", "CENTIPEDE"],
         initialOrderSequence: "6417235",
         theme: "Animals by limb count",
-        hint: ""
+        hint: "Natural extremes",
+        hint2: "Go out on a limb"
     },
     // Add more puzzles as needed
 ];
+
