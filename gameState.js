31925d217d74cf2260debb60cd99a96629b4ed51
awagner6 <37525104+awagner6@users.noreply.github.com>
2024-04-09 16:11:44 -0700

    Update gameState.js

--- gameState.js
+++ gameState.js
@@ -1,13 +1,15 @@
-export function saveGameState(boardStates, boardOrders, gameWon, reverseWon, currentPuzzleIndex, gameEnded, lightbulbUsed, streakCount) {
+export function saveGameState(boardStates, revBoardStates, boardOrders, gameWon, reverseWon, currentPuzzleIndex, gameEnded, lightbulbUsed, streakCount, revSolve) {
     const gameState = {
         boardStates: boardStates,
+        revBoardStates: revBoardStates,
         boardOrders: boardOrders,
         gameWon: gameWon,
         reverseWon: reverseWon,
         currentPuzzleIndex: currentPuzzleIndex,
-        gameEnded,
-        lightbulbUsed,
-        streakCount
+        gameEnded: gameEnded,
+        lightbulbUsed: lightbulbUsed,
+        streakCount: streakCount,
+        revSolve: revSolve
     };
     console.log(gameState)
     localStorage.setItem('gameState', JSON.stringify(gameState));
