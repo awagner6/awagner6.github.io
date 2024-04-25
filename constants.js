2b392458c9054fcb863a5bf7d9115457e7b5313a
awagner6 <37525104+awagner6@users.noreply.github.com>
2024-04-24 20:44:20 -0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -6,7 +6,8 @@ export const puzzles = [
         theme: "ROYGBIV",
         hint: "Take a bow",
         hint2: "Get colorful",
-        post_solve: ["Fire truck: Red", "Pumpkin: Orange", "Blonde: Yellow", "Crocodile: Green", "Sky: Blue", "Denim: Indigo", "Lavender: Violet"]
+        post_solve: ["Fire truck: Red", "Pumpkin: Orange", "Blonde: Yellow", "Crocodile: Green", "Sky: Blue", "Denim: Indigo", "Lavender: Violet"],
+        revAllowed: true
     },
     {
         id: 2,
@@ -15,7 +16,8 @@ export const puzzles = [
         theme: "Animals by limb count",
         hint: "Natural extremes",
         hint2: "Go out on a limb",
-        post_solve: ["SNAKE: 0", "FROG: 4", "STARFISH: 5", "ANT: 6", "SPIDER: 8", "CRAB: 10", "CENTIPEDE: ~30 (not 100!)" ]
+        post_solve: ["SNAKE: 0", "FROG: 4", "STARFISH: 5", "ANT: 6", "SPIDER: 8", "CRAB: 10", "CENTIPEDE: ~30 (not 100!)" ],
+        revAllowed: true
     },
     {
         id: 3,
@@ -24,7 +26,8 @@ export const puzzles = [
         theme: "Money in order of president",
         hint: "Face value",
         hint2: "Presidential procession",
-        post_solve: ["$1: Washington", "5¢: Jefferson", "$20: Jackson", "$5: Lincoln", "$50: Grant", "10¢: FDR", "50¢: JFK"]
+        post_solve: ["$1: Washington", "5¢: Jefferson", "$20: Jackson", "$5: Lincoln", "$50: Grant", "10¢: FDR", "50¢: JFK"],
+        revAllowed: true
     },
 
     {
@@ -34,7 +37,8 @@ export const puzzles = [
         theme: "Alphabetical letter homophone endings",
         hint: "Behind your ear",
         hint2: "Say the endings out loud",
-        post_solve: ["JANAE: A", "JACOBI: B", "JESSIE: C", "JODY: D", "JOEY: E", "JEFF: F", "JENJI: G"]
+        post_solve: ["JANAE: A", "JACOBI: B", "JESSIE: C", "JODY: D", "JOEY: E", "JEFF: F", "JENJI: G"],
+        revAllowed: false
     },
     // Add more puzzles as needed
 
@@ -45,7 +49,8 @@ export const puzzles = [
         theme: "Stripe count",
         hint: "What's my line?",
         hint2: "Solids' counterparts",
-        post_solve: ["8 BALL: 0", "CREDIT CARD: 1", "ARMY CORPORAL: 2", "NEAPOLITAN ICE CREAM: 3", "RAINBOW: 7", "AMERICAN FLAG: 13", "ZEBRA: Many"]
+        post_solve: ["8 BALL: 0", "CREDIT CARD: 1", "ARMY CORPORAL: 2", "NEAPOLITAN ICE CREAM: 3", "RAINBOW: 7", "AMERICAN FLAG: 13", "ZEBRA: Many"],
+        revAllowed: true
     },
     {
         id: 6,
@@ -54,7 +59,8 @@ export const puzzles = [
         theme: "Starts with a 31-day month abbr.",
         hint: "Ahead of time",
         hint2: "Cal train",
-        post_solve: [ "(Jan)itor", "(Mar)ble", "(May)onnaise", "(Jul)ienned", "(Aug)mentation", "(Oct)opus", "(Dec)ision"]
+        post_solve: [ "(Jan)itor", "(Mar)ble", "(May)onnaise", "(Jul)ienned", "(Aug)mentation", "(Oct)opus", "(Dec)ision"],
+        revAllowed: true
     },
     {
         id: 7,
@@ -63,7 +69,8 @@ export const puzzles = [
         theme: "Movie installment<br>within its franchise",
         hint: "Installments",
         hint2: "Franchise additions",
-        post_solve: ["James Bond 1", "Alien 2", "Die Hard 3", "Shrek 4", "M:I 5", "Rocky 6", "Star Wars 7"]
+        post_solve: ["James Bond 1", "Alien 2", "Die Hard 3", "Shrek 4", "M:I 5", "Rocky 6", "Star Wars 7"],
+        revAllowed: true
 
     },
     {
@@ -73,7 +80,8 @@ export const puzzles = [
         theme: "Number of syllables",
         hint: "What a mouthful!",
         hint2: "Count sounds",
-        post_solve: ["SCRUNCHED: 1", "BREAKTHROUGH: 2", "STRAIGHTFORWARD: 3", "ACKNOWLEDGEMENT: 4", "ENTREPRENEURSHIP: 5", "UNRECOGNIZABLE: 6", "CONFIDENTIALITY: 7"]
+        post_solve: ["SCRUNCHED: 1", "BREAKTHROUGH: 2", "STRAIGHTFORWARD: 3", "ACKNOWLEDGEMENT: 4", "ENTREPRENEURSHIP: 5", "UNRECOGNIZABLE: 6", "CONFIDENTIALITY: 7"],
+        revAllowed: true
     },
     {
         id: 9,
@@ -82,7 +90,8 @@ export const puzzles = [
         theme: "West <-> East U.S. states by latitudes",
         hint: "Coast to coast",
         hint2: "First two, last two",
-        post_solve: ["(WA)ITING GA(ME)", "(OR)OGE(NY)", "(CA)LAMA(RI)", "(CA)SCA(DE)", "(CA)SSA(VA)", "(CA)BERNET FRA(NC)", "(CA)YU(GA)"]
+        post_solve: ["(WA)ITING GA(ME)", "(OR)OGE(NY)", "(CA)LAMA(RI)", "(CA)SCA(DE)", "(CA)SSA(VA)", "(CA)BERNET FRA(NC)", "(CA)YU(GA)"],
+        revAllowed: false
     },
     {
         id: 10,
@@ -91,7 +100,8 @@ export const puzzles = [
         theme: "Preceded by a number",
         hint: "Figures out",
         hint2: "Missing numbers",
-        post_solve: ["(ONE) EIGHTIES", "(TWO) CENTS", "(THREE) CHEERS", "(FOUR) SEASONS", "(FIVE) SENSES", "(SIX) FLAGS", "(SEVEN) SEAS"]
+        post_solve: ["(ONE) EIGHTIES", "(TWO) CENTS", "(THREE) CHEERS", "(FOUR) SEASONS", "(FIVE) SENSES", "(SIX) FLAGS", "(SEVEN) SEAS"],
+        revAllowed: true
     },
     {
         id: 11,
@@ -100,7 +110,8 @@ export const puzzles = [
         theme: "Number of sides",
         hint: "So edgy!",
         hint2: "Shapes",
-        post_solve: ["DORITO: 3", "CHESSBOARD: 4", "FLAG OF NEPAL: 5", "HONEYCOMB CELL: 6", "STOP SIGN: 8", "LOONIE: 11", "GLOBE: 20 (not quite round!)"]
+        post_solve: ["DORITO: 3", "CHESSBOARD: 4", "FLAG OF NEPAL: 5", "HONEYCOMB CELL: 6", "STOP SIGN: 8", "LOONIE: 11", "GLOBE: 20 (not quite round!)"],
+        revAllowed: true
     },
     {
         id: 12,
@@ -109,7 +120,8 @@ export const puzzles = [
         theme: "E.U. languages<br>alphabetically (exonyms)",
         hint: "Linguistic ABCs",
         hint2: "Languages A-to-Z",
-        post_solve: ["Hello: English", "Bonjour: French", "Guten tag: German", "Buongiorno: Italian", "Tudo bem: Portuguese", "Hola: Spanish", "Hej: Swedish"]
+        post_solve: ["Hello: English", "Bonjour: French", "Guten tag: German", "Buongiorno: Italian", "Tudo bem: Portuguese", "Hola: Spanish", "Hej: Swedish"],
+        revAllowed: true
     },
     {
         id: 13,
@@ -118,7 +130,8 @@ export const puzzles = [
         theme: "Head count",
         hint: "Bean counter",
         hint2: "Head count",
-        post_solve: ["Headless Horseman: 0", "Marble bust: 1", "CatDog: 2", "Cerberus: 3", "Mount Rushmore: 4", "Hyrdra: 9", "Easter Island: 1,043"]        
+        post_solve: ["Headless Horseman: 0", "Marble bust: 1", "CatDog: 2", "Cerberus: 3", "Mount Rushmore: 4", "Hyrdra: 9", "Easter Island: 1,043"],
+        revAllowed: true        
     },
     {
         id: 14,
@@ -127,7 +140,8 @@ export const puzzles = [
         theme: "Prefixes of magnitude",
         hint: "SIzes",
         hint2: "Prefixes of magnitude",
-        post_solve: ["Tera: trillion", "Giga: billion", "Mega: million", "Kilo: thousand", "Micro: millionth", "Nano: billionth", "Pico: trillionth"]
+        post_solve: ["Tera: trillion", "Giga: billion", "Mega: million", "Kilo: thousand", "Micro: millionth", "Nano: billionth", "Pico: trillionth"],
+        revAllowed: true
     },
     {
         id: 15,
@@ -136,7 +150,8 @@ export const puzzles = [
         theme: "Frequencies:<br>happens every...",
         hint: "Often times",
         hint2: "Frequencies",
-        post_solve: ["Birthday: Every year", "Blue moon: 2-3 years", "World cup: 4 years", "US Census: 10 years", "Cicada emergence: 13 or 17 years", "Royal Jubilee: 25 years", "Halley's Comet flyby: 75-79 years"]
+        post_solve: ["Birthday: Every year", "Blue moon: 2-3 years", "World cup: 4 years", "US Census: 10 years", "Cicada emergence: 13 or 17 years", "Royal Jubilee: 25 years", "Halley's Comet flyby: 75-79 years"],
+        revAllowed: true
     },
     {
         id: 16,
@@ -145,7 +160,8 @@ export const puzzles = [
         theme: "Number of subdivisions in a game",
         hint: "Game breaks",
         hint2: "Quarters, periods, innings...",
-        post_solve: ["Sumo: 1 bout", "Soccer: 2 halves", "Hockey: 3 periods", "American football: 4 quarters", "Baseball: 9 innings", "Bowling: 10 frames", "Golf: 18 holes"]
+        post_solve: ["Sumo: 1 bout", "Soccer: 2 halves", "Hockey: 3 periods", "American football: 4 quarters", "Baseball: 9 innings", "Bowling: 10 frames", "Golf: 18 holes"],
+        revAllowed: true
     },
     {
         id: 17,
@@ -154,7 +170,8 @@ export const puzzles = [
         theme: "Hidden doubled musical scale",
         hint: "Duly noted",
         hint2: "The Sound of Music x2",
-        post_solve: ["(Dodo)", "P(rere)quisite", "Panto(mimi)ng", "Do yoursel(f a fa)vor", "Mi(so so)up", "Ma(lala)", "Pe(titi)on"]
+        post_solve: ["(Dodo)", "P(rere)quisite", "Panto(mimi)ng", "Do yoursel(f a fa)vor", "Mi(so so)up", "Ma(lala)", "Pe(titi)on"],
+        revAllowed: true
     },
     {
         id: 18,
@@ -163,7 +180,8 @@ export const puzzles = [
         theme: "Instrument string count",
         hint: "Plucky numbers",
         hint2: "Strings together",
-        post_solve: ["Erhu: 2", "Ukelele: 4", "Guitar: 6", "Mandolin: 8", "Sitar: 18-21", "Concert harp: 47", "Grand piano: ~230"]
+        post_solve: ["Erhu: 2", "Ukelele: 4", "Guitar: 6", "Mandolin: 8", "Sitar: 18-21", "Concert harp: 47", "Grand piano: ~230"],
+        revAllowed: true
     },
     {
         id: 19,
@@ -172,7 +190,8 @@ export const puzzles = [
         theme: "Non-numerical<br>number words",
         hint: "Word counts",
         hint2: "Non-number number words",
-        post_solve: ["Hat trick: 3", "Dozen: 12", "Baker's dozen: 13", "Score: 20", "Century: 100", "Gross: 144", "Grand: 1000"]
+        post_solve: ["Hat trick: 3", "Dozen: 12", "Baker's dozen: 13", "Score: 20", "Century: 100", "Gross: 144", "Grand: 1000"],
+        revAllowed: true
     },
     {
         id: 20,
@@ -181,7 +200,8 @@ export const puzzles = [
         theme: "Average amounts in the human body",
         hint: "Sum body",
         hint2: "Average amounts in the human body",
-        post_solve: ["Feet: 2", "Brain lobes: 6", "Fingers: 10", "Ribs: 24", "Teeth: 32", "Vertebrae: 33", "Chromosomes: 46"]       
+        post_solve: ["Feet: 2", "Brain lobes: 6", "Fingers: 10", "Ribs: 24", "Teeth: 32", "Vertebrae: 33", "Chromosomes: 46"],
+        revAllowed: true       
     },
     {
         id: 21,
@@ -190,7 +210,8 @@ export const puzzles = [
         theme: "Numbers, alphabetically",
         hint: "Numberheads",
         hint2: "First letters",
-        post_solve: ["Eight", "Four", "Nine", "One", "Six", "Ten", "Zero"]
+        post_solve: ["Eight", "Four", "Nine", "One", "Six", "Ten", "Zero"],
+        revAllowed: true
     },
     {
         id: 22,
@@ -199,7 +220,8 @@ export const puzzles = [
         theme: "Word chain from 'Word' to 'Chain'",
         hint: "Before and after",
         hint2: "2-word pairs",
-        post_solve: ["Word game", "Game animal", "Animal house", "House plant", "Plant food", "Food chain"]
+        post_solve: ["Word game", "Game animal", "Animal house", "House plant", "Plant food", "Food chain"],
+        revAllowed: false
     },
     {
         id: 23,
@@ -208,7 +230,8 @@ export const puzzles = [
         theme: "North to South America",
         hint: "On the map",
         hint2: "North to South",
-        post_solve: ["Mexico", "Guatemala", "Honduras", "Nicaragua", "Costa Rica", "Panama", "Colombia"]
+        post_solve: ["Mexico", "Guatemala", "Honduras", "Nicaragua", "Costa Rica", "Panama", "Colombia"],
+        revAllowed: false
     },
     {
         id: 24,
@@ -217,7 +240,8 @@ export const puzzles = [
         theme: "Holes/pockets/cavities",
         hint: "The pits",
         hint2: "W-hole numbers",
-        post_solve: ["Bagel: 1", "Nose: 2", "Bowling ball: 3", "Pool table: 6", "Wiffle ball: 8", "Croc shoe: 13", "Sponge: many"]        
+        post_solve: ["Bagel: 1", "Nose: 2", "Bowling ball: 3", "Pool table: 6", "Wiffle ball: 8", "Croc shoe: 13", "Sponge: many"],
+        revAllowed: true        
     },
     {
         id: 25,
@@ -226,7 +250,8 @@ export const puzzles = [
         theme: "Chronological ages by beginning",
         hint: "Eras tour",
         hint2: "Ages of history",
-        post_solve: ["Stone age: ~3.4 million BCE", "Bronze age: ~3000 BCE", "Iron age: ~1200 BCE", "Middle ages: ~500 CE", "Machine age: ~1880 CE", "Atomic age: 1945 CE", "Information age: ~1970s"]
+        post_solve: ["Stone age: ~3.4 million BCE", "Bronze age: ~3000 BCE", "Iron age: ~1200 BCE", "Middle ages: ~500 CE", "Machine age: ~1880 CE", "Atomic age: 1945 CE", "Information age: ~1970s"],
+        revAllowed: true
     },
     {
         id: 26,
@@ -235,7 +260,8 @@ export const puzzles = [
         theme: "Only-E words by word count",
         hint: "County census",
         hint2: "Check the vowels",
-        post_solve: ["Evergreen needle: 7", "Sleeveless tee: 6", "Render speechless: 5", "Best kept secret: 4", "Crescent wrench: 3", "Stress test: 2", "Strengths: 1"]
+        post_solve: ["Evergreen needle: 7", "Sleeveless tee: 6", "Render speechless: 5", "Best kept secret: 4", "Crescent wrench: 3", "Stress test: 2", "Strengths: 1"],
+        revAllowed: true
     },
     {
         id: 27,
@@ -244,7 +270,8 @@ export const puzzles = [
         theme: "Line-ending lyrics of Imagine",
         hint: "Imagine that!",
         hint2: "Lennon lyrics",
-        post_solve: ["Imagine there's NO HEAVEN", "It's easy if YOU TRY", "No hell BELOW US", "Above us, ONLY SKY", "Imagine all THE PEOPLE", "Living FOR TODAY", "OOOOOOH"]
+        post_solve: ["Imagine there's NO HEAVEN", "It's easy if YOU TRY", "No hell BELOW US", "Above us, ONLY SKY", "Imagine all THE PEOPLE", "Living FOR TODAY", "OOOOOOH"],
+        revAllowed: false
     },
     {
         id: 28,
@@ -253,7 +280,8 @@ export const puzzles = [
         theme: "Snack food items per package",
         hint: "Junk heap",
         hint2: "Amount per pack",
-        post_solve: ["Snickers: 1 bar", "Twix: 2 bars", "Hostess Hohos: 3 cakes", "Kitkat: 4 bars", "Starbursts: 12 squares", "Mentos: 14 discs", "Skittles: many shells"]
+        post_solve: ["Snickers: 1 bar", "Twix: 2 bars", "Hostess Hohos: 3 cakes", "Kitkat: 4 bars", "Starbursts: 12 squares", "Mentos: 14 discs", "Skittles: many shells"],
+        revAllowed: true
     },
     {
         id: 29,
@@ -262,7 +290,8 @@ export const puzzles = [
         theme: "MLB stat counts",
         hint: "MLB ballpark estimates",
         hint2: "Baseball counts",
-        post_solve: ["Players per team: 26-28", "Bat length: 35-37 in.", "Outs per game: 54 (or 51)", "Feet between bases: 90", "Avg game length: 158 min", "Games per season per team: 162", "Avg. stadium capacity: ~45,000"]
+        post_solve: ["Players per team: 26-28", "Bat length in inches: 35-37", "Outs per game: 54 (or 51)", "Feet between bases: 90", "Avg game length: 158 min", "Games per season per team: 162", "Avg. stadium capacity: ~45,000"],
+        revAllowed: true
     },
     {
         id: 30,
@@ -271,7 +300,8 @@ export const puzzles = [
         theme: "Peppers by Scoville heat level",
         hint: "Spice it up",
         hint2: "Scoville scale",
-        post_solve: ["Bell: 0", "Poblano: ~1,500", "Jalapeño: ~3,500", "Serrano: ~15,000", "Cayenne: ~40,000", "Habanero: ~250,000", "Ghost: ~1,000,000"]        
+        post_solve: ["Bell: 0", "Poblano: ~1,500", "Jalapeño: ~3,500", "Serrano: ~15,000", "Cayenne: ~40,000", "Habanero: ~250,000", "Ghost: ~1,000,000"],
+        revAllowed: true        
     },
     {
         id: 31,
@@ -280,6 +310,7 @@ export const puzzles = [
         theme: "Golf scores by stroke count",
         hint: "Course numbers",
         hint2: "Golf scores",
-        post_solve: ["Ace: hole in 1", "Condor: 4 under par", "Albatross: 3 under par", "Eagle: 2 under par", "Birdie: 1 under par", "Par", "Bogey: one over par"]        
+        post_solve: ["Ace: hole in 1", "Condor: 4 under par", "Albatross: 3 under par", "Eagle: 2 under par", "Birdie: 1 under par", "Par", "Bogey: one over par"],
+        revAllowed: true        
     }
 ];
