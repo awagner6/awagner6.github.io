dd6f0f51ad631dfcea7c5868dc2b275b62428864
awagner6 <37525104+awagner6@users.noreply.github.com>
2024-07-11 11:27:06 -0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -1135,12 +1135,12 @@ export const puzzles = [
     },
     {
         id: 114,
-        solution: ["CHEVRON SHAPE", "THE MARK OF ZORRO", "THE FLASH LOGO", "CONFOUNDED FACE EMOJI MOUTH", "PIKACHU'S TAIL", "SF LOMBARD STREET", "OLYMPIC SLALOM SKI GATES"],
+        solution: ["CHEVRON SHAPE", "THE MARK OF ZORRO", "THE FLASH LOGO", "CONFOUNDED FACE EMOJI MOUTH", "PIKACHU'S TAIL", "SF LOMBARD STREET", "OLYMPIC SLALOM COURSE"],
         initialOrderSequence: "4261735",
         theme: "Number of kinks/turns",
         hint: "Getting kinky",
         hint2: "Number of kinks/turns",
-        post_solve: ["Chevron: 1 kink", "Mark of Zorro (Z): 2 kinks", "The Flash logo: 4 kinks", "Confounded emoji mouth: 5 kinks", "Pikachu's tail: 6 kinks", "SF Lombard St: 8 turns", "Olympic Slalom gates: 40-75"],
+        post_solve: ["Chevron: 1 kink", "Mark of Zorro (Z): 2 kinks", "The Flash logo: 4 kinks", "Confounded emoji mouth: 5 kinks", "Pikachu's tail: 6 kinks", "SF Lombard St: 8 turns", "Olympic Slalom course: 40-75 gates"],
         revAllowed: true
     },
     {
